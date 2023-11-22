46e8a41c07e3e5b9558462fd6d578cc72e7e7917
HwangGwihwan <hkh001107@gmail.com>
2023-11-22 19:55:45 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        print("Hello World!")
+        print("Hello World2!")
         setContentView(R.layout.activity_main)
     }
 }
