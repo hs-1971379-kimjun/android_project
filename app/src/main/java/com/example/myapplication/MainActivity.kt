5f58ccf6bdae9f5860551d0044cef3df1a7cf494
HwangGwihwan <hkh001107@gmail.com>
2023-11-22 20:14:26 +0900

    Update MainActivity.kt
    ㅇㅇㅇ

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,8 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        print("Hello World! pull request 확인용")
-        //왜안댐
+        print("Hello World! pull request 확인용") // tlqkf
         setContentView(R.layout.activity_main)
     }
 }
