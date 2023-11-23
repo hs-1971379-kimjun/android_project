6a5b94f5a08d9737b8b68a5b5b37edc79581dfbd
Dohyun <104420796+chungguul@users.noreply.github.com>
2023-11-23 15:32:12 +0900

    Create ModifyScreenActivity.kt
    modifyScreenActivity 작성

---
+++ app/src/main/java/com/example/myapplication/Activity/ModifyScreenActivity.kt
@@ -0,0 +1,77 @@
+package com.example.myapplication.Activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityModfiyScreenBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class ModifyScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityModfiyScreenBinding
+    private lateinit var database: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityModfiyScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupActionBar()
+        setupUI()
+
+        binding.modify.setOnClickListener {
+            navigateToEditScreen()
+        }
+    }
+
+    private fun setupActionBar() {
+        supportActionBar?.apply {
+            title = "Modify Item"
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    private fun setupUI() {
+        val itemKey = intent.getStringExtra("itemKey").toString()
+        database = FirebaseDatabase.getInstance().getReference("Items").child(itemKey)
+
+        database.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val item = snapshot.getValue(ProductItem::class.java)
+                updateUI(item)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("Error", "Failed to load data")
+            }
+        })
+    }
+
+    private fun updateUI(item: ProductItem?) {
+        binding.apply {
+            title.text = item?.title
+            description.text = item?.description
+            price.text = item?.price
+            soldTF.text = item?.status
+            name.text = item?.seller
+        }
+    }
+
+    private fun navigateToEditScreen() {
+        val itemKey = intent.getStringExtra("itemKey").toString()
+        val editIntent = Intent(this, EditScreenActivity::class.java)
+        editIntent.putExtra("itemKey", itemKey)
+        startActivity(editIntent)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+}
