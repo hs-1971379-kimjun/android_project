e5255c002843ae15cd3f24f12c017fea007cff34
hs-1971379-KimJun <57721520+hs-1971379-kimjun@users.noreply.github.com>
2023-11-23 14:51:26 +0900

    write 클 수정
    ㅇ

--- app/src/main/java/com/example/myapplication/Activity/WritePostActivity.kt
+++ app/src/main/java/com/example/myapplication/Activity/WritePostActivity.kt
@@ -14,6 +14,8 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class WritePostActivity : AppCompatActivity() {
+
+    // Firebase 데이터베이스와 사용자 인터페이스 컴포넌트에 대한 늦은 초기화
     private lateinit var dbRef: DatabaseReference
     private lateinit var titleEditText: EditText
     private lateinit var descriptionEditText: EditText
@@ -25,51 +27,60 @@ class WritePostActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write_post)
 
+        // Firebase 데이터베이스 참조 설정
         dbRef = FirebaseDatabase.getInstance().getReference("Items")
 
-        val mAuth = FirebaseAuth.getInstance()
-        val currentUser: FirebaseUser? = mAuth.currentUser
+        // Firebase 인증 객체를 통해 현재 로그인된 사용자 정보 가져오기
+        val currentUser = FirebaseAuth.getInstance().currentUser
 
-        titleEditText = findViewById(R.id.editTextTitle)
-        descriptionEditText = findViewById(R.id.editTextDescription)
-        priceEditText = findViewById(R.id.editTextPrice)
-        backButton = findViewById(R.id.buttonBack)
-        backButton.setOnClickListener{
+        // UI 컴포넌트 초기화
+        initializeUI()
+
+        // 뒤로가기 버튼 이벤트
+        backButton.setOnClickListener {
             finish()
         }
-        uploadButton = findViewById(R.id.buttonUpload)
-        uploadButton.setOnClickListener {
-            val title = titleEditText.text.toString()
-            val description = descriptionEditText.text.toString()
-            val price = priceEditText.text.toString()
-            val userName = currentUser?.email
 
-            // 글 작성 및 업로드
-            uploadPost(title, description, price, userName)
+        // 업로드 버튼 이벤트
+        uploadButton.setOnClickListener {
+            uploadPost(
+                title = titleEditText.text.toString(),
+                description = descriptionEditText.text.toString(),
+                price = priceEditText.text.toString(),
+                seller = currentUser?.email
+            )
             finish()
         }
     }
 
-    private fun uploadPost(title: String?, description: String?, price: String?, seller: String?) {
-        val formattedPrice = "$price"
-        val itemStatus = "판매 중"
+    // UI 컴포넌트 초기화를 위한 별도의 함수
+    private fun initializeUI() {
+        titleEditText = findViewById(R.id.editTextTitle)
+        descriptionEditText = findViewById(R.id.editTextDescription)
+        priceEditText = findViewById(R.id.editTextPrice)
+        backButton = findViewById(R.id.buttonBack)
+        uploadButton = findViewById(R.id.buttonUpload)
+    }
 
-        val itemData = hashMapOf(
+    // 게시물 업로드 함수
+    private fun uploadPost(title: String, description: String, price: String, seller: String?) {
+        // 게시물 정보
+        val itemData = mapOf(
             "title" to title,
             "description" to description,
-            "price" to formattedPrice,
-            "status" to itemStatus,
+            "price" to "$price",
+            "status" to "판매 중",
             "seller" to seller
         )
 
+        // 새 게시물 참조 생성 및 데이터베이스에 업로드
         val newItemRef = dbRef.push()
         newItemRef.setValue(itemData)
             .addOnSuccessListener {
-                val documentId = newItemRef.key
-                Toast.makeText(this,"업로드 완료",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "업로드 완료", Toast.LENGTH_SHORT).show()
             }
-            .addOnFailureListener { e ->
-                Toast.makeText(this,"업로드 실패",Toast.LENGTH_SHORT).show()
+            .addOnFailureListener {
+                Toast.makeText(this, "업로드 실패", Toast.LENGTH_SHORT).show()
             }
     }
 }
