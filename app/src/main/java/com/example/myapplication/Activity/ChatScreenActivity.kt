28f77b44ba441bd9e21f97fff45d1085b53afa22
Dohyun <104420796+chungguul@users.noreply.github.com>
2023-12-05 02:25:26 +0900

    변경

--- app/src/main/java/com/example/myapplication/Activity/ChatScreenActivity.kt
+++ app/src/main/java/com/example/myapplication/Activity/ChatScreenActivity.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.Adapter.ChatAdapter
--- app/src/main/java/com/example/myapplication/Adapter/ChatAdapter.kt
+++ app/src/main/java/com/example/myapplication/Adapter/ChatAdapter.kt
@@ -1,61 +1,10 @@
-//package com.example.myapplication.Adapter
-//
-//import android.view.LayoutInflater
-//import android.view.View
-//
-//import android.view.ViewGroup
-//import android.widget.TextView
-//import androidx.recyclerview.widget.RecyclerView
-//import com.example.myapplication.R
-//
-//data class chatItem(
-//    val msg: String? = null,
-//    val time: Long? = null,
-//    var timeString: String? = null,
-//    val sender: String? = null,
-//    val receiver: String? = null
-//)
-//
-//class ChatAdapter (val itemList : ArrayList<chatItem>) : RecyclerView.Adapter<ChatAdapter.ChatViewHolder>() {
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
-//        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_message_display, parent, false)
-//        return ChatViewHolder(view)
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return itemList.count()
-//    }
-//
-//    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
-//        holder.msg.text = itemList[position].msg ?: "No"
-//        holder.time.text = itemList[position].timeString ?: "No time available"
-//    }
-//
-//    inner class ChatViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//        val msg: TextView = itemView.findViewById(R.id.chatText) ?: TextView(itemView.context)
-//        val time: TextView = itemView.findViewById(R.id.time) ?: TextView(itemView.context)
-//    }
-//}
-
 package com.example.myapplication.Adapter
 
 import android.view.LayoutInflater
-import android.view.View
-
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityMessageDisplayBinding
 
-//data class chatItem(
-//    val content: String? = null,
-//    val timestamp: Long? = null,
-//    var formattedTime: String? = null,
-//    val senderUsername: String? = null,
-//    val receiverUsername: String? = null
-//)
 data class chatItem(
     var sender: String? = null,  // sender 필드 추가
     var receiver: String? = null,  // receiver 필드 추가
